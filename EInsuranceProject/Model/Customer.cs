300ecca8629781130ad3a25b78d63856b47ac35d
Priyanka750-2001 <priyankagoravanakolla@gmail.com>
2023-10-02 12:50:47 +0530

    Added Annotations to all classes.

--- EInsuranceProject/Model/Customer.cs
+++ EInsuranceProject/Model/Customer.cs
@@ -7,22 +7,45 @@ namespace EInsuranceProject.Model
     {
         [Key]
         public int CustomerId { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string CustomerFirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string CustomerLastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
         public string Email { get;set; }
-        public Int32 Phone {  get; set; } 
+        [Required]
+        [Phone]
+        [StringLength(20)]
+        public Int32 Phone {  get; set; }
+
+        [StringLength(100)]
         public string Address { get; set; }
+
+        [Required]
         public State State { get; set; }
+
+        [StringLength(50)]
         public string City { get;set; }
+
+        [StringLength(100)]
         public string Nominee { get;set; }
+
+        [StringLength(50)]
         public string NomineeRelation { get;set; }
         public bool Status { get;set; }
 
         public List<Agent>? Agents { get; set; }
-       
 
-        public User? User { get; set; }
-        [ForeignKey("UserId")]
+        [ForeignKey("User")]
         public int UserId { get; set; }
+        public User? User { get; set; }
+      
     }
 }
--- EInsuranceProject/Model/Employee.cs
+++ EInsuranceProject/Model/Employee.cs
@@ -6,19 +6,29 @@ namespace EInsuranceProject.Model
     public class Employee
     {
         [Key]
-        public int EmployeeId { get; set; } 
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "Employee First Name is required.")]
+        [StringLength(50, ErrorMessage = "Employee First Name should not exceed 50 characters.")]
         public string EmployeeFirstName { get; set; }
+
+        [Required(ErrorMessage = "Employee Last Name is required.")]
+        [StringLength(50, ErrorMessage = "Employee Last Name should not exceed 50 characters.")]
         public string EmployeeLastName { get; set; }
+
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number should be a 10-digit number.")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
         public string Email { get;set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Salary must be greater than 0.")]
         public Double Salary { get;set; }
         public bool Status { get; set; }
 
 
-
-        public User? User { get; set; }
-        [ForeignKey("UserId")]
+        [ForeignKey("User")]
         public int UserId { get; set; }
-
+        public User? User { get; set; }      
     }
 }
